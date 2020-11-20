b2352189a3650127632635f0a3f95eecbd8d0dcd
druid <1837039333@qq.com>
2020-11-20 15:01:04 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-20 08:54:45
+ * @LastEditTime: 2020-11-20 14:20:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vuehr\src\App.vue
+-->
+<template>
+  <div id="app">
+    <!-- 路由匹配到的组件将显示在这里,rounte.js里面所有的组件都显示  -->
+    <router-view/>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,138 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import {
+    Button,
+    Input,
+    Table,
+    TableColumn,
+    Dialog,
+    Card,
+    Container,
+    Icon,
+    Select,
+    Form,
+    Tag,
+    Tree,
+    Pagination,
+    Badge,
+    Loading,
+    Message,
+    MessageBox,
+    Menu,
+    Tabs,
+    TabPane,
+    Breadcrumb,
+    BreadcrumbItem,
+    Dropdown,
+    Steps,
+    Step,
+    Tooltip,
+    Popover,
+    Collapse,
+    FormItem,
+    Checkbox,
+    Header,
+    DropdownMenu,
+    DropdownItem,
+    Aside,
+    Main,
+    MenuItem,
+    Submenu,
+    Option,
+    Col,
+    Row,
+    Upload,
+    Radio,
+    DatePicker,
+    RadioGroup,
+    CollapseItem,
+    Switch
+} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000};
+Vue.use(Switch);
+Vue.use(CollapseItem);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(DatePicker);
+Vue.use(Upload);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Option);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Header);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Checkbox);
+Vue.use(FormItem);
+Vue.use(Collapse);
+Vue.use(Popover);
+Vue.use(Menu);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Dropdown);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Tooltip);
+Vue.use(Tree);
+Vue.use(Pagination);
+Vue.use(Badge);
+Vue.use(Loading);
+Vue.use(Button);
+Vue.use(Input);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Dialog);
+Vue.use(Card);
+Vue.use(Container);
+Vue.use(Icon);
+Vue.use(Select);
+Vue.use(Form);
+Vue.use(Tag);
+Vue.prototype.$alert = MessageBox.alert
+Vue.prototype.$confirm = MessageBox.confirm
+
+// 导入 Utils 中定义的方法
+import {postRequest} from "./utils/api";
+import {postKeyValueRequest} from "./utils/api";
+import {putRequest} from "./utils/api";
+import {deleteRequest} from "./utils/api";
+import {getRequest} from "./utils/api";
+import {initMenu} from "./utils/menus";
+import 'font-awesome/css/font-awesome.min.css'
+
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.postKeyValueRequest = postKeyValueRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.getRequest = getRequest;
+
+Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+    if (to.path == '/') { // to 和 from 都是 Route 类型的
+        next();
+    } else {
+        if (window.sessionStorage.getItem("user")) {
+            initMenu(router, store); // 发送 get请求到 SystemConfigController 
+            next();
+        } else {
+            next('/?redirect=' + to.path); // 重定向
+        }
+    }
+})
+
+// 实例化一个 vue 对象，在 mount 生命周期就渲染，页面首先执行main.js
+new Vue({
+    router, // 路由
+    store, //根据实例化存储，子组件通过 this.$store 访问
+    render: h => h(App) // App组件渲染,这里的h即是 vm.$createElement
+}).$mount('#app')
